43a0d5a9e120cd1648c289d91d02a0852cd97cd1
Gungnir54 <vincent.becker@epitech.eu>
2022-07-09 10:34:22 +0200

    project transfer in public

---
+++ public/JS/hashtagedTweets.js
@@ -0,0 +1,11 @@
+import { linkHashtags } from "./linkHashtags.js"
+
+const tweetsContents = document.querySelectorAll(".tweet__content")
+
+for (const tweetContent of tweetsContents) {
+
+    const newContent = linkHashtags(tweetContent.textContent)
+    
+    tweetContent.innerHTML = newContent
+
+}
---
+++ public/JS/popup.js
@@ -0,0 +1,18 @@
+let user = document.getElementById('userPop')
+let pop = document.querySelector('#popup')
+let hide = document.getElementById('popup')
+
+user.addEventListener('click', () => {
+    popuppage = () => {
+        pop.style.display = "block";
+    }
+    popuppage()
+})
+hide.addEventListener('click', () => {
+    hidenpage = () => {
+        pop.style.display = "none";
+    }
+    hidenpage()
+})
+
+
